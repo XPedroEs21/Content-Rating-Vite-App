4130dc73747a56ce2631aaf8069148b36e3cb871
Pedro <xpedroes@gmail.com>
2024-07-06 23:44:15 -0600

    Added like and dislike functions and buttons with state

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -2,15 +2,44 @@
 import React, { Component } from 'react';
 import './ContentRating.css';
 
-class ContentRating extends Component {
+export class ContentRating extends Component {
   constructor() {
     super();
+    /*Initialize the states for the likes and dislikes, these are 0 for the initial state, also stablish te function to increment the number when button is clicked*/
+    this.state = {
+      likes: 0,
+      dislikes 0,
+
+      handlelike:() => {
+        this.setstate((prevstate) => ({
+          likes: prevstate.likes +1
+        }));
+      },
+      handledislike:() => {
+        this.setstate((prevstate) =? ({
+          dislikes: prevstate.dislikes + 1
+        }));
+      }
+    };
   }
   render() {
     return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
+     <div className='content-rating'>
+        <p>
+           I am learning how to use state and arrow functions, this case for buildin reacting buttons for like and dislike,help me to try them please!!
+        </p>
+        <div className='rating-buttons'>
+
+          <button className='like-button' onClick={this.state.handleLike}>
+            Like ({this.state.likes})
+          </button>
+
+          <button className='dislike-button' onClick={this.state.handleDislike}>
+            Dislike ({this,state,dislikes})
+          </button>
+
+        </div>
+      </div>
     );
   }
 }
